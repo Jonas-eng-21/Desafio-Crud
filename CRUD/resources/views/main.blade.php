0540a4a424bfbbcacec1e4a0e0a4a907da338502
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-07 11:52:57 -0300

    refactor(routing): use resource controller and integrate Vite assets
    Replace manual user routes with Route::resource in web.php
    
    Inject @vite directive into main layout for CSS and JS loading
    
    Standardize UserController naming convention (PascalCase)
    
    Update npm dependencies for frontend build pipeline

--- CRUD/resources/views/main.blade.php
+++ CRUD/resources/views/main.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD de funcionários</title>
+    <title>CRUD</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="container">
