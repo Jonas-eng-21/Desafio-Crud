803170d1cbc8c87280b8ef881a8bf9146e8b154b
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-06 22:31:15 -0300

    feat(users): implement initial user listing and controller scaffold
    Create userController with index method to fetch all users
    
    Add users.blade.php view for simple data display
    
    Define manual web routes for all user CRUD operations
    
    Rename default users migration to align with project timestamp

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', action:[HomeController::class, 'index'])->name('home');
+Route::get('/users', action:[UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', action:[UserController::class, 'create'])->name('users.create');
+Route::get('/users', action:[UserController::class, 'store'])->name('users.store');
+Route::get('/users{user}', action:[UserController::class, 'show'])->name('users.show');
+Route::get('/users{user}/edit', action:[UserController::class, 'edit'])->name('users.edit');
+Route::get('/users{user}', action:[UserController::class, 'update'])->name('users.update');
+Route::get('/users{user}', action:[UserController::class, 'destroy'])->name('users.destroy');
