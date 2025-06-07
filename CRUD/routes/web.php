307d74ef65b007627a40c184fe1a3466102e811a
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-07 14:53:54 -0300

    feat(employees): scaffold MVC structure and migration
    Generate FuncionarioController as a resource controller
    
    Create Funcionario model and initial migration table
    
    Register /funcionarios resource routes in web.php

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\FuncionarioController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::resource('/funcionarios', FuncionarioController::class);
 Route::resource('/users', UserController::class);
 Route::get('/', action:[HomeController::class, 'index'])->name('home');
+
 
