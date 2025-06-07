d3264a5a00f4b9d8ba676bdfd52fb6b4b4541f3a
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-07 16:49:51 -0300

    wwork in progress

--- CRUD/vite.config.js
+++ CRUD/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,10 +11,17 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/form.css',
                 'resources/css/funcionario-list.css',
-                'resources/js/dropdown-details.js',
+                'resources/js/funcionario-validation.js',
+                'resources/js/form-validation.js',
+                'resources/js/dropdown.js',
                 ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, "node_modules/bootstrap/dist"),
+        },
+    },
 });
