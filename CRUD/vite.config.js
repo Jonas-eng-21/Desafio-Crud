d2101d1d8e8458f3906ac423ea443c2d608a8128
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-08 14:21:34 -0300

    Corrigindo alguns erros de estilização

--- CRUD/vite.config.js
+++ CRUD/vite.config.js
@@ -1,27 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/form.css',
-                'resources/css/funcionario-list.css',
-                'resources/js/funcionario-validation.js',
-                'resources/js/form-validation.js',
-                'resources/js/dropdown.js',
-                ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, "node_modules/bootstrap/dist"),
-        },
-    },
 });
